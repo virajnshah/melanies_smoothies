3593e89c9f4f67fa3dd473b43770c850d91e9121
virajnshah <127373932+virajnshah@users.noreply.github.com>
2026-01-12 03:19:19 +0530

    Create streamlit_app_pending_order.py

---
+++ streamlit_app_pending_order.py
@@ -0,0 +1,45 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# # Write directly to the app
+st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+"""
+Orders that need to be filled.
+"""
+)
+
+# name_on_order = st.text_input("Name on Smoothie")
+# st.write("The name on your Smoothie will be: ", name_on_order)
+
+
+cnx=st.connect('snowflake')
+session=cnx.session()
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
+# st.dataframe(data=my_dataframe,use_container_width=True)
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+
+    if submitted:
+          
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset["_1"])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset["_1"]})]
+                    )
+            st.success('Order(s) Updated', icon = '👍')
+        except:
+            st.write('Something went wrong')
+else:
+    st.success('There are no pending orders right now', icon = '👍')
+    
